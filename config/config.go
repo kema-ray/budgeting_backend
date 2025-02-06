1d4b610a3ec22ed6859ed4bff90af93bbdc86a66
kema-ray <rachel.oyondi@roamtech.com>
2025-02-06 14:23:35 +0300

    did database configurations

---
+++ config/config.go
@@ -0,0 +1,35 @@
+package config
+
+import (
+	"os"
+	"fmt"
+	"gorm.io/gorm"
+	"github.com/joho/godotenv"
+	"gorm.io/driver/postgres"
+)
+
+// DB is the global database connection
+var DB *gorm.DB
+
+// ConnectDatabase initializes the database connection
+func ConnectDatabase () {
+	_= godotenv.Load() // Load .env file
+
+	dsn := fmt.Sprintf(
+		"host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
+		os.Getenv("DB_HOST"),
+		os.Getenv("DB_USER"),
+		os.Getenv("DB_PASSWORD"),
+		os.Getenv("DB_NAME"),
+		os.Getenv("DB_PORT"),
+	)
+
+	var err error
+	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("Failed to connect to the database!")
+	}
+
+	fmt.Println("📌 Database connected successfully!")
+	// DB.AutoMigrate(&models.User{}, &models.Budget{}, &models.Expense{})
+}
