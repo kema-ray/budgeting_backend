3e09697055a33928f85f5f7306621f56ae0fd6f2
kema-ray <rachel.oyondi@roamtech.com>
2025-02-10 10:07:01 +0300

    created a controller for auth

---
+++ controller/auth_controller.go
@@ -0,0 +1,58 @@
+package controller
+
+import (
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	"github.com/kema-ray/home-budgeting-app/config"
+	"github.com/kema-ray/home-budgeting-app/models"
+	"golang.org/x/crypto/bcrypt"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func Register(c *gin.Context) {
+	var user models.User
+	if err := c.ShouldBindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error})
+		return
+	}
+
+	hashedPassword, _ := bcrypt.GenerateFromPassword([]byte(user.Password), bcrypt.DefaultCost)
+	user.Password = string(hashedPassword)
+
+	if err := config.DB.Create(&user).Error; err != nil {
+		c.JSON( http.StatusInternalServerError, gin.H{"error": "Could not register user"})
+	}
+
+	c.JSON(http.StatusOK, gin.H{"message": "User Registered Successfully"})
+}
+
+func Login(c *gin.Context) {
+	var input models.User
+	var user models.User
+
+	if err := c.ShouldBindJSON(&input); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	config.DB.Where("email = ?", input.Email).First(&user)
+
+	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(input.Password)); err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid Credentials"})
+		return
+	}
+
+	// Generate JWT Token
+	claims := jwt.MapClaims{
+		"userID": user.ID,
+		"exp": time.Now().Add(time.Hour * 24).Unix(),
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, _ := token.SignedString([]byte(os.Getenv("JWT_SECRET")))
+
+	c.JSON(http.StatusOK, gin.H{"token": tokenString})
+}
